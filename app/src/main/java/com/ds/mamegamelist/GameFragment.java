5b93759458bf5ca08e74a6c27b9c16fe2411fd8e
Doosoon Choi <doosoon@gmail.com>
2017-12-17 02:28:06 +0900

    add logs to test Activity and Fragment lifecycle callback order

--- app/src/main/java/com/ds/mamegamelist/GameFragment.java
+++ app/src/main/java/com/ds/mamegamelist/GameFragment.java
@@ -1,9 +1,12 @@
 package com.ds.mamegamelist;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +23,9 @@ public class GameFragment extends Fragment {
     private GameRecyclerViewAdapter mAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        Log.d("Test", "onCreate of Fragment");
     }
 
     @Override
@@ -31,6 +36,20 @@ public class GameFragment extends Fragment {
         mGameRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         mAdapter = new GameRecyclerViewAdapter();
         mGameRecyclerView.setAdapter(mAdapter);
+
+        Log.d("Test", "onCreateView of Fragment");
         return v;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d("Test", "onAttach of Fragment");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d("Test", "onActivityCreated of Fragment");
     }
 }
--- app/src/main/java/com/ds/mamegamelist/MainActivity.java
+++ app/src/main/java/com/ds/mamegamelist/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends FragmentActivity {
 
@@ -13,12 +14,16 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("Test", "onCreate before fragment");
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
         if (fragment == null) {
             fragment = new GameFragment();
+            Log.d("Test", "onCreate fragment new");
             fm.beginTransaction().add(R.id.fragment_container, fragment)
                     .commit();
+            Log.d("Test", "onCreate fragment committed");
         }
+        Log.d("Test", "onCreate end");
     }
 }
